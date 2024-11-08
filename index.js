c49042dce12ffdc7bd098ea8cd62c85415d4c10a
unknown <11Rahulsinghrawat@gmail.com>
2024-11-08 16:04:35 +0530

    Added the landingPage

---
+++ index.js
@@ -0,0 +1,53 @@
+
+var cursorDiv = document.querySelector("#cursor")
+var enterCursor = document.querySelector("body")
+
+enterCursor.addEventListener("mousemove",function(dest){
+    gsap.to("#cursor",{
+        x:dest.x-35,
+        y:dest.y-35,
+        ease:"back.out"
+    })
+})
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#page3 h1",{
+    x : "-150vw",
+    scrollTrigger:{
+        trigger:"#page3",
+        scroller:"body",
+        start:"top 0%",
+        end :"top -100%",
+        scrub:2,
+        pin:true,
+    }
+})
+
+
+gsap.fromTo("#scrollVerticallyDown", 
+    { y: '-350vh' }, 
+    { 
+      y: '50vh', 
+      ease: "none",
+      scrollTrigger: {
+        trigger: '.scrollImageVertically',
+        start: "top bottom", 
+        end: "bottom top", 
+        scrub: true
+      } 
+    }
+);
+
+gsap.fromTo("#scrollVerticallyUp", 
+    { y: '250vh' }, 
+    { 
+      y: '-200vh', 
+      ease: "none",
+      scrollTrigger: {
+        trigger: '.scrollImageVertically',
+        start: "top bottom", 
+        end: "bottom top", 
+        scrub: true
+      } 
+    }
+);
