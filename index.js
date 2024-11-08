2823f6432ca1a2eb04c5db21db6f15c39cb0d15b
unknown <11Rahulsinghrawat@gmail.com>
2024-11-08 20:57:22 +0530

    Added navbar

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 
 var cursorDiv = document.querySelector("#cursor")
 var enterCursor = document.querySelector("body")
@@ -10,11 +11,39 @@ enterCursor.addEventListener("mousemove",function(dest){
     })
 })
 
-gsap.registerPlugin(ScrollTrigger);
-gsap.to("#page3 h1",{
+
+//Navbar show hide
+var menuIcon = document.querySelector('#menuIcon');
+var menuDiv = document.querySelector('#Navbar')
+var homePage = document.querySelector('#LandingPage')
+
+//Navbar timeline
+var tl = gsap.timeline();
+tl.from(".nav",{
+  x:150,
+  duration:0.7,
+  stagger:0.28,
+  opacity:0
+})
+
+menuIcon.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // You can also use 'auto' for an instant scroll
+  });
+
+  homePage.classList.toggle('d-none')
+  menuDiv.classList.toggle('d-none');
+  if (!menuDiv.classList.contains('d-none')) {
+    tl.restart(); // Restart animation each time menu is shown
+  }
+});
+
+
+gsap.to("#LandingPage h1",{
     x : "-150vw",
     scrollTrigger:{
-        trigger:"#page3",
+        trigger:"#LandingPage",
         scroller:"body",
         start:"top 0%",
         end :"top -100%",
